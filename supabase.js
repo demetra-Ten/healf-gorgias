ce7694bbdbced58ad9ffc9f246ffb1ef5e78f0b6
Demetra Tennant <demetratennant@Demetras-MacBook-Air.local>
2026-06-18 14:11:09 +0100

    Add Supabase credentials

--- supabase.js
+++ supabase.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://imtapqvlfjcnyrsmpatg.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_Da1eZE_zJ7FimcLHJmqU8g_FHDjJZ1M';
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
